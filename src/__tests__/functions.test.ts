50a15f00a71abe86f9c2b45b98f385440f95e4bf
nicoladaniello <nicola.danie@gmail.com>
2022-10-05 13:55:00 +0200

    Add test for closures

--- src/__tests__/functions.test.ts
+++ src/__tests__/functions.test.ts
@@ -8,13 +8,52 @@ describe("Eva", () => {
     const exp = parser.parse(`
         (begin
             (def square (x)
-                (* x x)
-            )
+                (* x x))
 
             (square 2)
         )
     `);
 
     expect(eva.eval(exp)).toBe(4);
+  });
+
+  it("should evaluate functions with complex bodies", () => {
+    const exp = parser.parse(`
+        (begin
+            (def calc (x y)
+                (begin
+                    (var z 30)
+                    (+ (* x y) z)
+                ))
+
+            (calc 10 20)
+        )
+    `);
+
+    expect(eva.eval(exp)).toBe(230);
+  });
+
+  it("should evaluate closures", () => {
+    const exp = parser.parse(`
+        (begin
+            (var value 100)
+            
+            (def calc (x y)
+                (begin
+                    (var z (+ x y))
+                    
+                    (def inner (foo)
+                        (+ (+ foo z) value))
+
+                    inner
+                ))
+
+            (var fn (calc 10 20))
+
+            (fn 30)
+        )
+    `);
+
+    expect(eva.eval(exp)).toBe(160);
   });
 });
