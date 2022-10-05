5fa1a4406011ba4d0cecd1269422aca5a7868fb2
nicoladaniello <nicola.danie@gmail.com>
2022-10-05 13:20:45 +0200

    Refactor blocks tests to use parser

--- src/__tests__/blocks.test.ts
+++ src/__tests__/blocks.test.ts
@@ -1,50 +1,62 @@
-import Eva from "../Eva";
-// @ts-ignore
 import evaParser from "../../parser/evaParser.js";
+import Eva from "../Eva";
 
 describe("Eva", () => {
   const eva = new Eva();
 
   it("should evaluate block scopes", () => {
-    const actual = eva.eval([
-      "begin",
-      ["var", "x", 10],
-      ["var", "y", 20],
-      ["+", ["*", "x", "y"], 30],
-    ]);
+    const exp = evaParser.parse(`
+      (begin
+        (var x 10)  
+        (var y 20)  
+        (+ (* x y) 30)  
+      )
+    `);
 
-    expect(actual).toBe(230);
+    expect(eva.eval(exp)).toBe(230);
   });
 
   it("should evaluate nested blocks", () => {
-    const actual = eva.eval([
-      "begin",
-      ["var", "x", 10],
-      ["begin", ["var", "x", 20]],
-      "x",
-    ]);
+    const exp = evaParser.parse(`
+      (begin
+        (var x 10)
+        (begin
+          (var x 20)
+        )
+        x  
+      )
+    `);
 
-    expect(actual).toBe(10);
+    expect(eva.eval(exp)).toBe(10);
   });
 
   it("should allow nested blocks to access parent scopes", () => {
-    const actual = eva.eval([
-      "begin",
-      ["var", "value", 10],
-      ["var", "result", ["begin", ["var", "x", ["+", "value", 10]], "x"]],
-      "result",
-    ]);
+    const exp = evaParser.parse(`
+      (begin
+        (var value 10)
+        (var result (begin
+          (var x (+ value 10))
+          x
+        ))
+        result
+      )
+    `);
 
-    expect(actual).toBe(20);
+    expect(eva.eval(exp)).toBe(20);
   });
 
   it("should allow nested blocks to modify parent scopes", () => {
-    const actual = eva.eval([
-      "begin",
-      ["var", "data", 10],
-      ["begin", ["set", "data", 100]],
-      "data",
-    ]);
+    const exp = evaParser.parse(`
+      (begin
+        (var data 10)
+        (begin
+          (set data 100)  
+        )
+        data
+      )
+    `);
+
+    expect(eva.eval(exp)).toBe(100);
   });
 
   it("should allow nested blocks to modify parent scopes", () => {
@@ -55,12 +67,18 @@ describe("Eva", () => {
         (+ (* x 10) y)
       )
     `);
-    expect(eva.eval(exp)).toStrictEqual(120);
+    expect(eva.eval(exp)).toBe(120);
   });
 
   it("should throw if nested block tries to modify an undefined variable", () => {
-    const actual = () => eva.eval(["begin", ["begin", ["set", "data", 100]]]);
+    const exp = evaParser.parse(`
+      (begin
+        (begin
+          (set data 100)
+        )
+      )
+    `);
 
-    expect(actual).toThrowError();
+    expect(() => eva.eval(exp)).toThrowError();
   });
 });
